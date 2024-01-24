cb790e150ccff17ed6c147859a56778f3cbddf12
Yan Fung <fungmingyan@hotmail.com>
2024-01-24 00:22:51 +0000

    Displays NewsletterSignup and Reviews components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
+import NewsletterSignup from "./NewsletterSignup";
+import Reviews from "./Reviews";
 import "../styles/app.scss";
+
 
 const App = () => {
   return (
-    <div></div>
+    <>
+      <div className="App">
+        <NewsletterSignup />
+        <Reviews />
+      </div>
+    </>
   );
 };
 
